e0481f9bf5c596bcb8339483f7e120443750a353
Maximilian Mackh <mmackh@gmail.com>
2015-01-11 10:25:26 +0100

    Initial Commit

---
+++ IPDFCameraViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IPDFCameraViewController
+//
+//  Created by Maximilian Mackh on 11/01/15.
+//  Copyright (c) 2015 Maximilian Mackh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IPDFCameraViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IPDFCameraViewController
+//
+//  Created by Maximilian Mackh on 11/01/15.
+//  Copyright (c) 2015 Maximilian Mackh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
